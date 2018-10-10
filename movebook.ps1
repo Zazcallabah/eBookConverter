8926e0a1ad048cd2709f56642ef952e289ba12e0
Peter Hamberg <peter.hamberg@academicwork.se>
2018-10-10 12:38:16 +0200

    use publishfolder setting in movebook script

--- movebook.ps1
+++ movebook.ps1
@@ -1,15 +1,52 @@
 param($bookname,$target="archive")
 
-$kindlelib = Resolve-Path "..\..\books\kindle_library"
-$lib = Resolve-Path "..\..\books\library"
+
+$script:settingsFile = "$PSScriptRoot\settings.json"
+
+function LoadSettings
+{
+	if( ! (Test-Path $script:settingsFile) )
+	{
+		@{
+			"sourceEbooks" = "`$PSScriptRoot\storage\ebooks";
+			"sourceKindle" = "`$(`$Env:UserProfile)\Documents\My Kindle Content\";
+			"decryptedStorage" = "`$PSScriptRoot\storage\decryptedkindle";
+			"library" = "`$PSScriptRoot\output\ebooks";
+			"kindleLibrary" = "`$PSScriptRoot\output\ebooks";
+			"publishFolder" = "`$PSScriptRoot\published";
+		} | ConvertTo-Json | SC -Path $script:settingsFile
+	}
+	$script:settings = gc -Raw $script:settingsFile | ConvertFrom-Json
+}
+
+function GetSettingsPath
+{
+	param($key)
+	
+	$ExecutionContext.InvokeCommand.ExpandString($script:settings."$key") 
+}
+
+function NormalizePath
+{
+	param($path)
+	if( !(Test-Path $path))
+	{
+		mkdir $path
+	}
+	Resolve-Path $path
+}
+
+$kindlelib = NormalizePath (GetSettingsPath "kindleLibrary")
+$lib = NormalizePath (GetSettingsPath "library")
+
 $result = @()
 $result += ls "$kindlelib\$bookname*"
 $result += ls "$lib\$bookname*"
 
-$outfolder = "\\omicron\backup\books\"
+$outfolder = GetSettingsPath "publishFolder"
 if( Test-Path $outfolder )
 {
-	$result | ?{ !(Test-Path "$outfolder$target\$($_.Name)") } | %{
+	$result | select -Unique | ?{ !(Test-Path "$outfolder$target\$($_.Name)") } | %{
 		Write-Host "Publishing $($_.Name) to $target"
 		cp -Recurse -Force $_.FullName "$outfolder$target"
 	}
